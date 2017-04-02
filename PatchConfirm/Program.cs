86107d1d8848d7ca8f3bc1c939c928a4a98867f1
h1tb1t <h1tb1t@noreply.github.com>
2017-04-02 15:12:24 +0100

    Created

--- PatchConfirm/Program.cs
+++ PatchConfirm/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WUApiInterop;
 
 namespace PatchConfirm
 {
@@ -10,7 +12,22 @@ namespace PatchConfirm
     {
         static void Main(string[] args)
         {
+            var updateServiceManager = new UpdateServiceManager();
+            var updateSession = new UpdateSession();
+            var UpdateService = updateServiceManager.AddScanPackageService("Offline sync Service", Directory.GetCurrentDirectory()+"\\wsusscn2.cab", 1);
+            var updateSearcher = updateSession.CreateUpdateSearcher();
 
+            updateSearcher.ServerSelection = ServerSelection.ssOthers;
+            updateSearcher.ServiceID = UpdateService.ServiceID;
+
+            Console.WriteLine("Searching...");
+            var SearchResult = updateSearcher.Search("IsInstalled=0");
+            for (int i =0; i < SearchResult.Updates.Count-1; i++)
+            {
+                Console.WriteLine(SearchResult.Updates[i].Title);
+            }
+            Console.WriteLine("Search completed!");
+            Console.ReadLine();
         }
     }
 }
